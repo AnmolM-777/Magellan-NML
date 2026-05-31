11cce1d915a0c2f9814e50d8dccf3ab340b33b8a
Anmol Mishra <anmolindia2006@gmail.com>
2026-05-31 14:20:00 +0530

    feat: add switching energy parameter to MQCA init

--- simulator.py
+++ simulator.py
@@ -1,3 +1,3 @@
 class MQCAModel:
-    def __init__(self):
-        pass
+    def __init__(self, energy_per_magnet_switch_aJ=2.0):
+        self.energy_per_magnet_switch_aJ = energy_per_magnet_switch_aJ
