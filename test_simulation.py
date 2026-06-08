11c151b58dcdf418f1feebf46684dbf191c78127
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-08 10:00:00 +0530

    test: add CMOS baseline tests

--- test_simulation.py
+++ test_simulation.py
@@ -1,6 +1,10 @@
-from simulator import MQCAModel
+from simulator import MQCAModel, CMOSModel
 
 def test_multiplier():
     model = MQCAModel()
     specs = model.get_multiplier_specs(2)
     assert specs['magnets'] == 120
+
+def test_cmos():
+    cmos = CMOSModel()
+    assert cmos.get_mac_energy_J(8) == 1.5e-13
