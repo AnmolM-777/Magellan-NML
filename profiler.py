1f6d29bed0afbefd3a5acda8c12de133a3b59d46
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-09 17:30:00 +0530

    feat: add YOLOv8-nano layer configurations placeholder

--- profiler.py
+++ profiler.py
@@ -3,3 +3,5 @@ class WorkloadProfiler:
         self.models = ['MiDaS', 'YOLOv8-nano']
     def get_midas_layers(self):
         return [{'layer_id': 'midas.stem.conv', 'macs': 150000000}]
+    def get_yolo_layers(self):
+        return [{'layer_id': 'yolo.stem.conv', 'macs': 44236800}]
