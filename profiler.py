c13941aac981fe134b1b8fbb2ad5c83e4d21eb82
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-09 14:40:00 +0530

    feat: add MiDaS layer configurations placeholder

--- profiler.py
+++ profiler.py
@@ -1,3 +1,5 @@
 class WorkloadProfiler:
     def __init__(self):
         self.models = ['MiDaS', 'YOLOv8-nano']
+    def get_midas_layers(self):
+        return [{'layer_id': 'midas.stem.conv', 'macs': 150000000}]
