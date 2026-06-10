1232d17c5decce5d52efbbc632d9b223a66e32d3
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-10 10:15:00 +0530

    feat: expand profiler static database with input/output tensor shapes

--- profiler.py
+++ profiler.py
@@ -1,7 +1,8 @@
 class WorkloadProfiler:
     def __init__(self):
-        self.models = ['MiDaS', 'YOLOv8-nano']
-    def get_midas_layers(self):
-        return [{'layer_id': 'midas.stem.conv', 'macs': 150000000}]
-    def get_yolo_layers(self):
-        return [{'layer_id': 'yolo.stem.conv', 'macs': 44236800}]
+        pass
+    def get_fallback_profile(self):
+        return [
+            {'layer_id': 'midas.stem.conv', 'model': 'MiDaS', 'type': 'Conv2d', 'macs': 150000000, 'weights_count': 3120, 'input_shape': (3, 256, 256), 'output_shape': (32, 128, 128)},
+            {'layer_id': 'yolo.stem.conv', 'model': 'YOLOv8-nano', 'type': 'Conv2d', 'macs': 44236800, 'weights_count': 432, 'input_shape': (3, 640, 640), 'output_shape': (16, 320, 320)}
+        ]
