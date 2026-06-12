38d578ae0ad53ac681b98379e531ce18826cc58a
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-12 14:15:00 +0530

    feat: map computational steps using array size configuration

--- mapper.py
+++ mapper.py
@@ -1,6 +1,7 @@
 from simulator import MQCAModel, CMOSModel
 
 class WorkloadMapper:
-    def __init__(self):
+    def __init__(self, array_size=1024):
         self.mqca = MQCAModel()
         self.cmos = CMOSModel()
+        self.array_size = array_size
