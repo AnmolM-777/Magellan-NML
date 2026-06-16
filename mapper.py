be32612edd25a6d2d4be6d95ed3594db11a80e91
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-16 15:45:00 +0530

    feat: write loop to map all profiled layers

--- mapper.py
+++ mapper.py
@@ -6,3 +6,14 @@ class WorkloadMapper:
         self.cmos = CMOSModel()
         self.array_size = array_size
         self.mem_energy_J_per_byte = mem_energy_pJ_per_byte * 1e-12
+
+    def map_workload(self, profiled_layers, bit_width=8):
+        results = []
+        for l in profiled_layers:
+            # Basic calculation
+            nml_energy = l['macs'] * 1e-18
+            results.append({
+                'layer_id': l['layer_id'],
+                'nml_total_energy_J': nml_energy
+            })
+        return results
