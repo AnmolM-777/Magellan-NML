03f1b1ad37f0ed8f6a5bb1237e5796d3bbe0471f
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-20 16:30:00 +0530

    feat: add ranking analyzer to mapper.py

--- mapper.py
+++ mapper.py
@@ -13,18 +13,23 @@ class WorkloadMapper:
         for l in profiled_layers:
             macs = l['macs']
             mem = l['total_memory_bytes']
-            # Compute NML
             nml_compute = macs * mqca_mac_specs['energy_J']
             mem_energy = mem * self.mem_energy_J_per_byte
             nml_total = nml_compute + mem_energy
-            # CMOS
             cmos_compute = macs * self.cmos_model.get_mac_energy_J(bit_width)
             cmos_total = cmos_compute + mem_energy
             results.append({
                 'layer_id': l['layer_id'],
                 'model': l['model'],
+                'macs': macs,
+                'total_memory_bytes': mem,
+                'arithmetic_intensity': l['arithmetic_intensity'],
                 'nml_total_energy_J': nml_total,
                 'cmos_total_energy_J': cmos_total,
                 'total_energy_savings': cmos_total / nml_total if nml_total > 0 else 1.0
             })
         return results
+
+    def analyze_rankings(self, mapped_results):
+        sorted_by_savings = sorted(mapped_results, key=lambda x: x['total_energy_savings'], reverse=True)
+        return sorted_by_savings[:3], sorted_by_savings[-3:]
