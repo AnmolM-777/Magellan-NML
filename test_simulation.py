080ecd4c53108121d5dfd1db1adc89866b2a0fab
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-21 13:50:00 +0530

    test: update unit tests with mapper integration assertions

--- test_simulation.py
+++ test_simulation.py
@@ -1,5 +1,6 @@
 from simulator import MQCAModel, CMOSModel
 from profiler import WorkloadProfiler
+from mapper import WorkloadMapper
 
 def test_multiplier():
     model = MQCAModel()
@@ -10,8 +11,8 @@ def test_cmos():
     cmos = CMOSModel()
     assert cmos.get_mac_energy_J(8) == 1.5e-13
 
-def test_profiler_footprint():
-    prof = WorkloadProfiler()
-    layers = prof.run_profiler(bit_width=8)
-    # Check that memory bytes are correctly populated
-    assert layers[0]['total_memory_bytes'] > 0
+def test_mapper_calculations():
+    mapper = WorkloadMapper()
+    layers = [{'layer_id': 'test', 'model': 'test', 'macs': 1000, 'total_memory_bytes': 100, 'arithmetic_intensity': 10}]
+    res = mapper.map_workload(layers)
+    assert len(res) == 1
