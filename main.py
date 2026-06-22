3e45966582abf578465367f49a14c2e09a35a12c
Anmol Mishra <anmolindia2006@gmail.com>
2026-06-22 14:10:00 +0530

    feat: link profiler and mapper inside main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Magellan-NML main script loaded.')
+from profiler import WorkloadProfiler
+from mapper import WorkloadMapper
+
+if __name__ == '__main__':
+    prof = WorkloadProfiler()
+    mapper = WorkloadMapper()
+    layers = prof.run_profiler()
+    mapped = mapper.map_workload(layers)
+    print(f'Mapped {len(mapped)} layers.')
