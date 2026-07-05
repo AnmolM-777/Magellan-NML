e028b61f4d0715f3e411003236257dc30e5eeb64
Anmol Mishra <anmolindia2006@gmail.com>
2026-07-05 10:00:00 +0530

    feat: create formatted table printer function in main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from profiler import WorkloadProfiler
-from mapper import WorkloadMapper
-from roofline import RooflineAnalyzer
-
-if __name__ == '__main__':
-    prof = WorkloadProfiler()
-    mapper = WorkloadMapper()
-    analyzer = RooflineAnalyzer()
-    layers = prof.run_profiler()
-    mapped = mapper.map_workload(layers)
-    table = analyzer.run_sensitivity(mapped, 102.4e9, 204.8e9)
-    print(table)
+def format_table(headers, rows):
+    for h in headers:
+        print(h, end=' | ')
+    print()
